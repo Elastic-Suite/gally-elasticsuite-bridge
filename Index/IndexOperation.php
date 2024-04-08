d6209ac6b4513c5dd3cbafce2dc8b36952f0858c
Pierre Gauthier <pigau@smile.fr>
2024-04-08 14:58:08 +0200

    [7.4] Compatibility

--- Index/IndexOperation.php
+++ Index/IndexOperation.php
@@ -203,7 +203,7 @@ class IndexOperation extends BaseIndexOperation
         }
 
         /** @var IndexDetails $index */
-        $index = $this->client->query(IndexApi::class, 'refreshIndexItem', name: $index->getName(), index: []);
+        $index = $this->client->query(IndexApi::class, 'refreshIndexItem', $index->getName(), []);
 
         return $index;
     }
@@ -218,7 +218,7 @@ class IndexOperation extends BaseIndexOperation
         }
 
         /** @var IndexDetails $index */
-        $index = $this->client->query(IndexApi::class, 'installIndexItem', name: $index->getName(), index: []);
+        $index = $this->client->query(IndexApi::class, 'installIndexItem', $index->getName(), []);
 
         return $index;
     }
